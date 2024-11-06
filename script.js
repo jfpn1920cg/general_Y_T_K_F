9822038035829f78aa26f0ed441fd7028ae6229b
jfpn1920cr <162055100+jfpn1920cg@users.noreply.github.com>
2024-11-05 23:08:17 -0500

    se_agrego_la_barra_de_busqueda

---
+++ script.js
@@ -0,0 +1,29 @@
+//funcionalidad_menu_de_navegacion
+
+//funcionalidad_carrusel
+let slideIndex = 1;
+showSlides(slideIndex);
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
